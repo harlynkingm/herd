433d4580b278808db1e73f52db373a01efb9be0b
Max Harlynking <mharlynk@andrew.cmu.edu>
2015-12-12 16:06:17 -0500

    Try again

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.set('views', __dirname + '/public/views');
 // Define the view (templating) engine
 app.set('view engine', 'ejs');
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 
