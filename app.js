119b8f7913bcb6d324dfdfe9f64f6f309129e76d
Max Harlynking <mharlynk@andrew.cmu.edu>
2015-12-12 16:43:08 -0500

    Changed server startup

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 
-http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+app.listen(app.get('port') ,app.get('ip'), function () {
     console.log("Express server listening at %s:%d ", app.get('ip'),app.get('port'));
-    server();
 });
