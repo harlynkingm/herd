1a25123e089e9be5327e15673e6e9e57a97b3488
Max Harlynking <mharlynk@andrew.cmu.edu>
2015-12-12 20:30:04 -0500

    I dont even know

--- models/mongoModel.js
+++ models/mongoModel.js
@@ -127,11 +127,12 @@ findSoundcloudSongs = function(songs){
         }, {}, function(data){
             counter++;
             if (data[0]){
-                console.log(data[0]);
                 var url = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + data[0].id + "&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true";
                 var obj = {contentId: data[0].id, type:'music', name:data[0].title, url:url, views: data[0].playback_count, comments_count:0};
                 hypemEntry.push(obj);
+                console.log(counter);
                 if (counter == songs.length - 1){
+                    console.log("ASDFLKASHDJFLKAJDFLKASJDFKLAJSKFDJLASKJDFLAKJ");
                     mongoDB.collection('content').insertMany(hypemEntry, function(err, status){
                         console.log("HYPEM MUSIC INSERTED");
                     });
