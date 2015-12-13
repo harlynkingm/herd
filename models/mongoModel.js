5ac9bfe2e8b42799154ee754a61812db45417b6d
Max Harlynking <mharlynk@andrew.cmu.edu>
2015-12-12 20:28:39 -0500

    Moved the if statment screw that

--- models/mongoModel.js
+++ models/mongoModel.js
@@ -131,11 +131,11 @@ findSoundcloudSongs = function(songs){
                 var url = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" + data[0].id + "&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true";
                 var obj = {contentId: data[0].id, type:'music', name:data[0].title, url:url, views: data[0].playback_count, comments_count:0};
                 hypemEntry.push(obj);
-            }
-            if (counter == songs.length - 1){
-                mongoDB.collection('content').insertMany(hypemEntry, function(err, status){
-                    console.log("HYPEM MUSIC INSERTED");
-                });
+                if (counter == songs.length - 1){
+                    mongoDB.collection('content').insertMany(hypemEntry, function(err, status){
+                        console.log("HYPEM MUSIC INSERTED");
+                    });
+                }
             }
         });
     }
